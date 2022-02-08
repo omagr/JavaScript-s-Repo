f064b4fdd0587e7d5c2c709d4815da99d420f0e0
omag <omag.about@gmail.com>
2022-02-08 20:08:56 +0530

    Comments

--- Index.js
+++ Index.js
@@ -1,1 +1,6 @@
-console.log("Installation")
+// This is an in-line comment.
+
+/* This is a
+multi-line comment */
+
+NOTE: As you write code, you should regularly add comments to clarify the function of parts of your code. Good commenting can help communicate the intent of your code—both for others and for your future self.
