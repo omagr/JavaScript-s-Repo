71e084c8e4311c882db91cbae91d4e8cb7ea7c5d
omag <omag.about@gmail.com>
2022-02-08 20:36:18 +0530

    Variables

--- Index.js
+++ Index.js
@@ -1,6 +1,42 @@
-// This is an in-line comment.
+// Declare JavaScript Variables
+var Name;
+var _name;
+var $name;
+var name;
+var name99;
 
-/* This is a
-multi-line comment */
+// Storing Values with the Assignment Operator
+ourName = 5;
 
-NOTE: As you write code, you should regularly add comments to clarify the function of parts of your code. Good commenting can help communicate the intent of your code—both for others and for your future self.
+// Assigning the Value of One Variable to Another
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar;
+
+// Initializing Variables with the Assignment Operator
+var myVar = 0;
+
+// Declare String Variables
+var myName = "your name";
+
+// Understanding Uninitialized Variables
+    /*  When JavaScript variables are declared, they have an initial value of undefined. If you do a mathematical operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a string with an undefined variable, you will get a literal string of undefined. */
+
+
+// Understanding Case Sensitivity in Variables
+var someVariable;
+var SomeVariable;
+var anotherVariableName;
+var AnotherVariableNamE;
+    /* Write variable names in JavaScript in camelCase. In camelCase, multi-word variable names have the first word in lowercase and the first letter of each subsequent word is capitalized.*/
+
+// Explore Differences Between the var and let Keywords
+var camper = "James";
+var camper = "David";
+let damper = "David";
+damper = "James";
+
+// Declare a Read-Only Variable with the const Keyword
+const FAV_PET = "Cats";
+    /* Note: It is common for developers to use uppercase variable identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays). You will learn more about objects, arrays, and immutable and mutable values in later challenges. Also in later challenges, you will see examples of uppercase, lowercase, or camelCase variable identifiers. */
