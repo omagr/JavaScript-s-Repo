f9c49186d1cd9c06286c4aaf6f2c7e31b7e545d3
omag <omag.about@gmail.com>
2022-02-12 13:15:33 +0530

    Loops

--- Index.js
+++ Index.js
@@ -1,175 +1,110 @@
-// Build JavaScript Objects
-/* Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties. Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.*/
-const Object = {
-  PROPERTIES: VALUES,
-  "name": "Whiskers",
-  "legs": 4.78,
-  "tails": 1,
-  "enemies": ["Water", "Dogs"],
-  make: "Ford",
-  5: "five",
-  "model": "focus"
-};
-
-
-// Accessing Object Properties with Dot Notation
-var testObj = {
-  "hat": "ballcap",
-  "s hirt": "jersey",
-  "soes": "cleats"
-};
-var hatValue = testObj.hat;      // ballcap
-var shirtValue = testObj["s hirt"];    // jersey
-
-
-
-// Accessing Object Properties with Variables
-/* Another use of bracket notation on objects is to access a property which is stored as the value of a variable. This can be very useful for iterating through an object's properties or when accessing a lookup table. */
-const someObj = {
-  propName: "John"
-};
-function propPrefix(str) {
-  const s = "prop";
-  return s + str;
+// Iterate with JavaScript While Loops
+/* The first type of loop we will learn is called a while loop because it runs while a specified condition is true and stops once that condition is no longer true.*/
+var myArray = [];
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++;
 }
-const someProp = propPrefix("Name");
-console.log(someObj[someProp]);
+console.log(myArray);
 
 
-// Updating Object Properties
-var myDog = {
-  "name": "Coder",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["freeCodeCamp Campers"]
-};
-myDog.name = "Happy Coder";
-myDog["legs"] = "Happy Coder";
 
-
-// Add New Properties to a JavaScript Object
-var myDog = {
-  "name": "Happy Coder",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["freeCodeCamp Campers"]
-};
-myDog['bark'] = "woof!"
-myDog.dark = "ooof!"
-
-
-// Delete Properties from a JavaScript Object
-const ourDog = {
-  "name": "Camper",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["everything!"],
-  "bark": "bow-wow"
-};
-delete ourDog.bark;
-
-
-// Using Objects for Lookups
-/* Objects can be thought of as a key/value storage, like a dictionary. If you have tabular data, you can use an object to lookup values rather than a switch statement or an if/else chain. This is most useful when you know that your input data is limited to a certain range. */
-function phoneticLookup(val) {
-  var result = "";
-  var lookup = {
-    "alpha": "Adams",
-    "bravo": "Boston",
-    "charlie": "Chicago",
-    delta: "Denver",
-    "echo": "Easy",
-    "foxtrot": "frank"
-  };
-  result = lookup[val];
-  return result;
+// Iterate with JavaScript For Loops
+/* for (a; b; c), where a is the initialization statement, b is the condition statement, and c is the final expression. */
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
 }
-console.log(phoneticLookup("foxtrot"));
-console.log(phoneticLookup("delta"));
-
-
-
-
-// Testing Objects for Properties
-/* Sometimes it is useful to check if the property of a given object exists or not. We can use the .hasOwnProperty(propname) method of objects to determine if that object has the given property name. .hasOwnProperty() returns true or false if the property is found or not. */
-const myObj = {
-  top: "hat",
-  bottom: "pants"
-};
-myObj.hasOwnProperty("top");
-myObj.hasOwnProperty("middle");
 
 
 
 
 
+// Iterate Odd/Even Numbers With a For Loop
+var ourArray = [];
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+console.log(ourArray);
+var myArray = [];
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+console.log(myArray);
 
 
-// Manipulating Complex Objects
-/* Sometimes you may want to store data in a flexible Data Structure. A JavaScript object is one way to handle flexible data. They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects. */
-var myMusic = [
-  {
-    "artist": "Billy Joel",
-    "title": "Piano Man",
-    "release_year": 1973,
-    "formats": [
-      "CD",
-      "8T",
-      "LP"
-    ],
-    "gold": true
-  },
-  // Add record here
-  {
-    "artist": "Beau Carnes",
-    "title": "Cereal Man",
-    "release_year": 2003,
-    "formats": [
-      "YouTube video"
-    ]
+
+
+// Count Backwards With a For Loop
+var ourArray = [];
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+console.log(ourArray);
+var myArray = [];
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+console.log(myArray);
+
+
+
+// Iterate Through an Array with a For Loop
+/* Remember that arrays have zero-based indexing, which means the last index of the array is length - 1. Our condition for this loop is i < arr.length, which stops the loop when i is equal to length. In this case the last iteration is i === 4 i.e. when i becomes equal to arr.length - 1 and outputs 6 to the console. Then i increases to 5, and the loop terminates because i < arr.length is false */
+const arr = [10, 9, 8, 7, 6];
+for (let i = 0; i < arr.length; i++) {
+   console.log(arr[i]);
+}
+let total = 0;
+for(let i = 0; i < arr.length; i++){
+  total = total + arr[i];
+}
+console.log(total);
+
+
+
+
+// Nesting For Loops
+/* If you have a multi-dimensional array, you can use the same logic as the prior waypoint to loop through both the array and any sub-arrays. Here is an example: */
+function multiplyAll(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++){
+    for (let j =0; j < arr[i].length; j++){
+      product = product * arr[i][j]
   }
-];
+}
+  return product;
+}
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+
+// Iterate with JavaScript Do...While Loops
+/* The next type of loop you will learn is called a do...while loop. It is called a do...while loop because it will first do one pass of the code inside the loop no matter what, and then continue to run the loop while the specified condition evaluates to true. */
+var myArray = [];
+var i = 10;
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5)
+console.log(i, myArray);
 
 
 
-// Accessing Nested Objects
-/* The sub-properties of objects can be accessed by chaining together the dot or bracket notation. */
-const ourStorage = {
-  "desk": {
-    "drawer": "stapler"
-  },
-  "cabinet": {
-    "top drawer": { 
-      "folder1": "a file",
-      "folder2": "secrets"
-    },
-    "bottom drawer": "soda"
+// Replace Loops using Recursion
+/* Recursion is the concept that a function can be expressed in terms of itself. To help understand this, start by thinking about the following task: multiply the first n elements of an array to create the product of those elements. Using a for loop, you could do this: */
+function multiply(arr, n) {
+  let product = 1;
+  for (let i = 0; i < n; i++) {
+    product *= arr[i];
   }
-};
-ourStorage.cabinet["top drawer"].folder2;
-ourStorage.desk.drawer;
-
-
-// Accessing Nested Arrays
-var myPlants = [
-  { 
-    type: "flowers",
-    list: [
-      "rose",
-      "tulip",
-      "dandelion"
-    ]
-  },
-  {
-    type: "trees",
-    list: [
-      "fir",
-      "pine",
-      "birch"
-    ]
-  }  
-];
-// Only change code below this line
-var secondTree = myPlants[1].list[1]; // Change this line
-console.log(secondTree)
-
+  return product;
+}
+/* The recursive version of multiply breaks down like this. In the base case, where n <= 0, it returns 1. For larger values of n, it calls itself, but with n - 1. That function call is evaluated in the same way, calling multiply again until n <= 0. At this point, all the functions can return and the original multiply returns the answer. Note: Recursive functions must have a base case when they return without calling the function again (in this example, when n <= 0), otherwise they can never finish executing.*/
+function multiply(arr, n) {
+  if (n <= 0) {
+    return 1;
+  } else {
+    return multiply(arr, n - 1) * arr[n - 1];
+  }
+}
