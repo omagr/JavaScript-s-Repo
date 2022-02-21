d14e5a4c2f84589558616967e47c1f70ea28c357
om agarwal <omag.about@gmail.com>
2022-02-21 14:46:57 +0530

    ES6 - Destructuring Arrays/Objects

--- Index.js
+++ Index.js
@@ -1,77 +1,84 @@
 // ES6 - ECMAScript 6
+// destruturing arrays and objects
 
-
-
-// Use Arrow Functions to Write Concise Anonymous Functions
-/* In JavaScript, we often don't need to name our functions, especially when passing a function as an argument to another function. Instead, we create inline functions. We don't need to name these functions because we do not reuse them anywhere else.*/
-/* arrow function syntax: */
-const myFunc = () => {
-  const myVar = "value";
-  return myVar;
-}
-/* When there is no function body, and only a return value, arrow function syntax allows you to omit the keyword return as well as the brackets surrounding the code. This helps simplify smaller functions into one-line statements: */
-const myFunc = () => "value";
-
-
-
-// Write Arrow Functions with Parameters
-/* ust like a regular function, you can pass arguments into an arrow function. */
-const doubler = (item) => item * 2;
-doubler(4); // 8
-/* If an arrow function has a single parameter, the parentheses enclosing the parameter may be omitted. */
-const doubler = item => item * 2;
-doubler(4); // 8
-/* It is possible to pass more than one argument into an arrow function. */
-const multiplier = (item, multi) => item * multi;
-multiplier(4, 2); // 8
-
-
-
-// Set Default Parameters for Your Functions
-/* The default parameter kicks in when the argument is not specified (it is undefined). As you can see in the example above, the parameter name will receive its default value Anonymous when you do not provide a value for the parameter. You can add default values for as many parameters as you want. */
-const greeting = (name = "Anonymous") => "Hello " + name;
-console.log(greeting("John"));
-console.log(greeting());
-
-
-
-// Use the Rest Parameter with Function Parameters
-/* In order to help us create more flexible functions, ES6 introduces the rest parameter for function parameters. With the rest parameter, you can create functions that take a variable number of arguments. These arguments are stored in an array that can be accessed later from inside the function. The console would display the strings You have passed 3 arguments. and You have passed 4 arguments.. The rest parameter eliminates the need to check the args array and allows us to apply map(), filter() and reduce() on the parameters array. */
-function howMany(...args) {
-  return "You have passed " + args.length + " arguments.";
-}
-console.log(howMany(0, 1, 2));
-console.log(howMany("string", null, [1, 2, 3], { }));
-
-const sum = (...args ) => {
-  let total = 0;
-  for( let i = 0; i < args.length; i++){
-    total = total + args[i]
-  }
-  
-}
-console.log(sum(2,46,5 ,5,6,6,6,7,78,6,5467,7,77)); //5718
-
-
-
-// Use the Spread Operator to Evaluate Arrays In-Place
-/* ES6 introduces the spread operator, which allows us to expand arrays and other expressions in places where multiple parameters or elements are expected. */
-const arr = [6, 89, 3, 45];
-/* ...arr returns an unpacked array. In other words, it spreads the array. However, the spread operator only works in-place, like in an argument to a function or in an array literal. The following code will not work: */
-const spreaded = ...arr;
-
-var maximus = Math.max.apply(null, arr);
-/* We had to use Math.max.apply(null, arr) because Math.max(arr) returns NaN. Math.max() expects comma-separated arguments, but not an array. The spread operator makes this syntax much better to read and maintain. */
-const maximus = Math.max(...arr); //89
-
-const arr1 = ['MAR', 'APR', 'MAY'];
-const arr2 = ['JUNE', 'JULY', 'AUGUEST'];
-let arr3;
-arr3 = ['JAN', 'FEB',...arr1,["JUNE",'JULY',...arr2], arr1];  // Change this line
-console.log(arr3);
-
+// Use Destructuring Assignment to Extract Values from Objects
 const OBJ1 = { name:"om", age:20, nationality:"indian"};
-const OBJ2 = { lastName:"agarwal", height:6, aukaat:"high"};
-let OBJ3;
-OBJ3 = { ...OBJ1, qualification:"bits", degree:20, ...OBJ2, OBJ2}; // Change this line
-console.log(OBJ3);
+const { name, age, nationality} = OBJ1; 
+console.log(`HAY! YO ${name}, ${age}, ${nationality}`)
+/* Here, the name and age variables will be created and assigned the values of their respective values from the user object. You can see how much cleaner this is.*/
+
+
+
+// Use Destructuring Assignment to Assign Variables from Objects
+/* Destructuring allows you to assign a new variable name when extracting values. You can do this by putting the new name after a colon when assigning the value. */
+const OBJ1 = { name:"om", age:20, nationality:"indian"};
+const { name:myname, age:myagre, nationality:mynationality} = OBJ1; 
+console.log(`HAY! YO ${myname}, ${agmyagree}, ${mynationality}`)
+/* You may read it as "get the value of user.name and assign it to a new variable named userName" and so on. The value of userName would be the string John Doe, and the value of userAge would be the number 34. */
+
+
+
+// Use Destructuring Assignment to Assign Variables from Nested Objects
+const user = {
+  name:"om", 
+  age:20, 
+  nationality:"indian",
+  johnDoe: { 
+    age: 34,
+    email: 'johnDoe@freeCodeCamp.com'
+  }
+};
+const { name, age, nationality, johnDoe} = user;
+console.log(`HAY! YO ${name}, ${age}, ${nationality}, ${johnDoe.age}`)
+/* And here's how you can assign an object properties' values to variables with different names: */
+const { johnDoe: { age: userAge, email: userEmail }} = user;
+/* example */
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
+console.log(lowToday)
+
+
+
+// Use Destructuring Assignment to Assign Variables from Arrays
+/* One key difference between the spread operator and array destructuring is that the spread operator unpacks all contents of an array into a comma-separated list. Consequently, you cannot pick or choose which elements you want to assign to variables. */
+const [a, b] = [1, 2, 3, 4, 5, 6];
+console.log(a, b); 
+/* The variable a is assigned the first value of the array, and b is assigned the second value of the array. We can also access the value at any index in an array with destructuring by using commas to reach the desired index: */
+const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
+console.log(a, b, c);
+/* Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a. */
+let a = 8, b = 6;
+[b , a] = [a, b]
+
+
+
+// Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+/* In some situations involving array destructuring, we might want to collect the rest of the elements into a separate array. */
+const source = [1,2,3,4,5,6,7,8,9,10];
+let [ a, b,,,c, ...rest ] = source
+console.log(c)
+/* Variables a and b take the first and second values from the array. After that, because of the rest parameter's presence, arr gets the rest of the values in the form of an array. The rest element only works correctly as the last variable in the list. As in, you cannot use the rest parameter to catch a subarray that leaves out the last element of the original array. */
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  const [a,b,...arr] = list;
+  return arr;
+}
+const arr = removeFirstTwo(source);
+
+
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const profileUpdate = ({ name, age, nationality, location }) => { }
+/* This effectively destructures the object sent into the function. This can also be done in-place: */
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+const half = ({ max, min }) => (max + min) / 2.0;
