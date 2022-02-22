0d30f9165968f19e7f654237f03e3f6e09c6c0ee
om agarwal <omag.about@gmail.com>
2022-02-22 12:31:16 +0530

    ES6 - Declarative Syntax

--- Index.js
+++ Index.js
@@ -1,84 +1,100 @@
 // ES6 - ECMAScript 6
-// destruturing arrays and objects
+// 
 
-// Use Destructuring Assignment to Extract Values from Objects
-const OBJ1 = { name:"om", age:20, nationality:"indian"};
-const { name, age, nationality} = OBJ1; 
-console.log(`HAY! YO ${name}, ${age}, ${nationality}`)
-/* Here, the name and age variables will be created and assigned the values of their respective values from the user object. You can see how much cleaner this is.*/
-
-
-
-// Use Destructuring Assignment to Assign Variables from Objects
-/* Destructuring allows you to assign a new variable name when extracting values. You can do this by putting the new name after a colon when assigning the value. */
-const OBJ1 = { name:"om", age:20, nationality:"indian"};
-const { name:myname, age:myagre, nationality:mynationality} = OBJ1; 
-console.log(`HAY! YO ${myname}, ${agmyagree}, ${mynationality}`)
-/* You may read it as "get the value of user.name and assign it to a new variable named userName" and so on. The value of userName would be the string John Doe, and the value of userAge would be the number 34. */
+// Create Strings using Template Literals
+/* A new feature of ES6 is the template literal. This is a special type of string that makes creating complex strings easier. Template literals allow you to create multi-line strings and to use string interpolation features to create strings. */
+const person = {
+  name: "Zodiac Hasbro",
+  age: 56
+};
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+console.log(greeting); // Hello, my name is Zodiac Hasbro! 
+                      //  and I am 56 years old..
+/* A lot of things happened there. Firstly, the example uses backticks (`), not quotes (' or "), to wrap the string. Secondly, notice that the string is multi-line, both in the code and the output. This saves inserting \n within strings. The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore. To add variables to strings, you just drop the variable in a template string and wrap it with ${ and }. Similarly, you can include other expressions in your string literal, for example ${a + b}. This new way of creating strings gives you more flexibility to create robust strings. */
 
 
 
-// Use Destructuring Assignment to Assign Variables from Nested Objects
-const user = {
-  name:"om", 
-  age:20, 
-  nationality:"indian",
-  johnDoe: { 
-    age: 34,
-    email: 'johnDoe@freeCodeCamp.com'
+
+
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+/* ES6 adds some nice support for easily defining object literals. */
+const getMousePosition = (x, y) => ({ x, y });
+/* getMousePosition is a simple function that returns an object containing two properties. ES6 provides the syntactic sugar to eliminate the redundancy of having to write x: x. You can simply write x once, and it will be converted tox: x (or something equivalent) under the hood. Here is the same function from above rewritten to use this new syntax: */
+
+
+
+
+
+// Write Concise Declarative Functions with ES6
+/* With ES6, you can remove the function keyword and colon altogether when defining functions in objects. Here's an example of this syntax: */
+const person = {
+  name: "Taylor",
+  sayHello() {
+    return `Hello! My name is ${this.name}.`;
   }
 };
-const { name, age, nationality, johnDoe} = user;
-console.log(`HAY! YO ${name}, ${age}, ${nationality}, ${johnDoe.age}`)
-/* And here's how you can assign an object properties' values to variables with different names: */
-const { johnDoe: { age: userAge, email: userEmail }} = user;
-/* example */
-const LOCAL_FORECAST = {
-  yesterday: { low: 61, high: 75 },
-  today: { low: 64, high: 77 },
-  tomorrow: { low: 68, high: 80 }
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
 };
-const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
-console.log(lowToday)
+bicycle.setGear(3);
+console.log(bicycle.gear); 
 
 
 
-// Use Destructuring Assignment to Assign Variables from Arrays
-/* One key difference between the spread operator and array destructuring is that the spread operator unpacks all contents of an array into a comma-separated list. Consequently, you cannot pick or choose which elements you want to assign to variables. */
-const [a, b] = [1, 2, 3, 4, 5, 6];
-console.log(a, b); 
-/* The variable a is assigned the first value of the array, and b is assigned the second value of the array. We can also access the value at any index in an array with destructuring by using commas to reach the desired index: */
-const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
-console.log(a, b, c);
-/* Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a. */
-let a = 8, b = 6;
-[b , a] = [a, b]
 
 
 
-// Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
-/* In some situations involving array destructuring, we might want to collect the rest of the elements into a separate array. */
-const source = [1,2,3,4,5,6,7,8,9,10];
-let [ a, b,,,c, ...rest ] = source
-console.log(c)
-/* Variables a and b take the first and second values from the array. After that, because of the rest parameter's presence, arr gets the rest of the values in the form of an array. The rest element only works correctly as the last variable in the list. As in, you cannot use the rest parameter to catch a subarray that leaves out the last element of the original array. */
-const source = [1,2,3,4,5,6,7,8,9,10];
-function removeFirstTwo(list) {
-  const [a,b,...arr] = list;
-  return arr;
+// Use class Syntax to Define a Constructor Function
+/* ES6 provides a new syntax to create objects, using the class keyword. It should be noted that the class syntax is just syntax, and not a full-fledged class-based implementation of an object-oriented paradigm, unlike in languages such as Java, Python, Ruby, etc. In ES5, we usually define a constructor function and use the new keyword to instantiate an object.*/
+var SpaceShuttle = function(targetPlanet){
+  this.targetPlanet = targetPlanet;
 }
-const arr = removeFirstTwo(source);
+var zeus = new SpaceShuttle('Jupiter');
+/* The class syntax simply replaces the constructor function creation:*/
+class SpaceShuttle {
+  constructor(targetPlanet) {
+    this.targetPlanet = targetPlanet;
+  }
+}
+const zeus = new SpaceShuttle('Jupiter');
+/* The class syntax simply replaces the constructor function creation */
+/* It should be noted that the class keyword declares a new function, to which a constructor is added. This constructor is invoked when new is called to create a new object. Note: UpperCamelCase should be used by convention for ES6 class names, as in SpaceShuttle used above. The constructor method is a special method for creating and initializing an object created with a class. You will learn more about it in the Object Oriented Programming section of the JavaScript Algorithms And Data Structures Certification. */
+// Only change code below this line
+class Vegetable  {
+  constructor(veg) {
+    this.name = veg;
+  }
+}
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
 
 
-// Use Destructuring Assignment to Pass an Object as a Function's Parameters
-const profileUpdate = ({ name, age, nationality, location }) => { }
-/* This effectively destructures the object sent into the function. This can also be done in-place: */
-const stats = {
-  max: 56.78,
-  standard_deviation: 4.34,
-  median: 34.54,
-  mode: 23.87,
-  min: -0.75,
-  average: 35.85
-};
-const half = ({ max, min }) => (max + min) / 2.0;
+
+
+
+
+// Use getters and setters to Control Access to an Object
+/* You can obtain values from an object and set the value of a property within an object. These are classically called getters and setters. Getter functions are meant to simply return (get) the value of an object's private variable to the user without the user directly accessing the private variable. Setter functions are meant to modify (set) the value of an object's private variable based on the value passed into the setter function. This change could involve calculations, or even overwriting the previous value completely. */
+class Book {
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer() {
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor) {
+    this._author = updatedAuthor;
+  }
+}
+const novel = new Book('anonymous');
+console.log(novel.writer);
+novel.writer = 'newAuthor';
+console.log(novel.writer);
+/* The console would display the strings anonymous and newAuthor. Notice the syntax used to invoke the getter and setter. They do not even look like functions. Getters and setters are important because they hide internal implementation details. Note: It is convention to precede the name of a private variable with an underscore (_). However, the practice itself does not make a variable private. */
